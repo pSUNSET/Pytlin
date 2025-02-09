c095131364c295256ebc7c1eda3d8ebfabc293c1
NoodleInWater <why097729@gmail.com>
2025-02-09 23:16:43 +0800

    Update times function to Array

--- src/main/kotlin/net/psunset/kwargs/collections/CollectionOp.kt
+++ src/main/kotlin/net/psunset/kwargs/collections/CollectionOp.kt
@@ -5,42 +5,52 @@ package net.psunset.kwargs.collections
  *
  * For example:
  * ```
- * val arr = mutableListOf(0, 1, 2)
- * val l = arr * 2
- * println(arr) // Result: [0, 1, 2]
- * // It stands for arr isn't changed though you use this function or operator.
- * println(l) // Result: [0, 1, 2, 0, 1, 2]
+ * val list = mutableListOf(0, 1, 2)
+ * val list2 = list * 2
+ * println(list) // Result: [0, 1, 2]
+ * // It stands for list isn't changed though you use this function or operator.
+ * println(list2) // Result: [0, 1, 2, 0, 1, 2]
  * // It is successful!
  * ```
- * @return [EmptyList] if `t` is less than 1; otherwise, A new list which has `t` times elements in the raw collection.
+ * @return [EmptyList] if `n` is less than 1; otherwise, A new list which has `n` elements in the raw collection.
  */
-operator fun <T: Any> Iterable<T>.times(t: Int): List<T> {
-    if (t <= 0) {
-        return emptyList()
-    }
-
-    if (t == 1) {
-        return this.toList()
-    }
+operator fun <T: Any> Iterable<T>.times(n: Int): List<T> {
+    if (n <= 0) return emptyList()
+    if (n == 1) return this.toList()
 
     val newList = this.toMutableList()
-    for (i in 1 until t) {
-        newList.addAll(this)
+    for (i in 1 until n) {
+        newList += this
     }
     return newList
 }
 
 /**
+ * The feature is same as [Iterable.times] function which is preceding this function.
+ * But make it available in an array.
+ */
+inline operator fun <reified T: Any> Array<T>.times(n: Int): Array<T> {
+    if (n <= 0) throw IllegalArgumentException("`n` should be a positive value because the length of an array should be greater than 0.")
+    if (n == 1) return this.clone()
+
+    val newList = this.toMutableList()
+    for (i in 1 until n) {
+        newList += this
+    }
+    return newList.toTypedArray()
+}
+
+/**
  * Idea from python.
- * If `t` is less than 1, this mutable list object will be empty and convert into a [List];
+ * If `n` is less than 1, this mutable list object will be empty and convert into a [List];
  * otherwise, this list will be stretch and the data inside is duplicated.
  * For example:
  * ```
- * val arr = mutableListOf(0, 1, 2)
- * arr *= 3
- * println(arr) // Result: [0, 1, 2, 0, 1, 2, 0, 1, 2]
- * arr *= 0
- * println(arr) // Result: []
+ * val list = mutableListOf(0, 1, 2)
+ * list *= 3
+ * println(list) // Result: [0, 1, 2, 0, 1, 2, 0, 1, 2]
+ * list *= 0
+ * println(list) // Result: []
  * ```
  * In the example, I use `val` instead `var`, but it's still working after I call `*=` operator.
  * It's because that we make the list directly update the elements without changing the list's pointer.
@@ -48,28 +58,25 @@ operator fun <T: Any> Iterable<T>.times(t: Int): List<T> {
  * But If you're using an immutable list, the compiler will crash.
  * Because, in fact, it calls [Iterable.times] first, and then update its pointer.
  * ```
- * val arr = listOf(0, 1, 2)
- * arr *= 3 // Error: val cannot be reassigned
+ * val list = listOf(0, 1, 2)
+ * list *= 3 // Error: val cannot be reassigned
  * ```
  * There are two solutions: one is using `var` instead, the other is make the list a mutable list by [toMutableList]
  * ```
- * var arr = listOf(0, 1, 2)
+ * var list = listOf(0, 1, 2)
  * // or
- * val arr = listOf(0, 1, 2).toMutableList()
+ * val list = listOf(0, 1, 2).toMutableList()
  * ```
  */
-operator fun <T: Any> MutableList<T>.timesAssign(t: Int) {
-    if (t <= 0) {
+operator fun <T: Any> MutableList<T>.timesAssign(n: Int) {
+    if (n <= 0) {
         this.removeAll { true }
         return
     }
-
-    if (t == 1) {
-        return
-    }
+    if (n == 1) return
 
     val elements = this.toList()
-    for (i in 1 until t) {
+    for (i in 1 until n) {
         this.addAll(elements)
     }
 }
--- src/test/kotlin/net/psunset/kwargs/Test.kt
+++ src/test/kotlin/net/psunset/kwargs/Test.kt
@@ -1,8 +1,5 @@
 package net.psunset.kwargs
 
-import net.psunset.kwargs.collections.kwargsOf
-import net.psunset.kwargs.lang.times
-
 class Test {
     /**
      * Just for test
